93eec96a477b4fe89c5f77aafcdf0c756b2c5d95
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-12-15 13:57:20 +0000

    Implemented Filtering by Categories and Subcategories, and Introduced Sorting by Price and Rating Features

--- products/views.py
+++ products/views.py
@@ -1,42 +1,83 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category, Subcategory
+
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-
     products = Product.objects.all()
     query = None
+    categories = None
+    subcategories = None
+    sort = None
+    direction = None
+    var_category = None
+    var_subcategory = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            var_category = request.GET['category']
+            categories = request.GET['category'].split(',')
+
+            if 'subcategory' in request.GET:
+                var_subcategory = request.GET['subcategory']
+                subcategories = request.GET['subcategory'].split(',')
+                products = products.filter(category__name__in=categories, subcategory__name__in=subcategories)
+                subcategories = Subcategory.objects.filter(name__in=subcategories)
+
+            else:
+                products = products.filter(category__name__in=categories)
+
+            categories = Category.objects.filter(name__in=categories)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
             # ADD DESCRIPTION TO QUERY ?????
             queries = Q(name__icontains=query)
             products = products.filter(queries)
 
+    # current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
+        'current_subcategories': subcategories,
+        'var_category': var_category,
+        'var_subcategory': var_subcategory,
     }
 
     return render(request, 'products/products.html', context)
+
 
 
 def product_detail(request, product_id):
     """ A view to show individual product details"""
 
     product = get_object_or_404(Product, pk=product_id)
-
     context = {
         'product': product,
     }
-
     return render(request, 'products/product_detail.html', context)
-
-
