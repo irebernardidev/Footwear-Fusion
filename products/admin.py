f29f764ee36c814249ff57831ac627888ffe7a2c
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-12-15 12:00:19 +0000

    Implemented the Products Application: Added New Models and Fixtures, and Customized the Admin Interface

---
+++ products/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import Product, Category, Subcategory
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Subcategory, SubcategoryAdmin)
