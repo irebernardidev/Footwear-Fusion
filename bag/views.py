18cf4e6cb61d2faf0c81d641a18c4b7aa09b159d
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-12-22 16:05:25 +0100

    Enhanced Bag Functionality and UI Updates: Implemented 'Adjust Bag' and 'Remove Item' Views, Integrated 'Calculate Subtotal' Template Filter
    Corrected URL Paths in Bag App's URLs File, and Introduced Toast Notifications for Improved User Feedback.

--- bag/views.py
+++ bag/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 def view_bag(request):
-    """ A view to render the shopping bag page """
+    """ A view that renders the bag contents page """
+    range_quantity = range(1, 21)
+    context = {
+        'range_quantity': range_quantity,
+    }
 
-    return render(request, 'bag/bag.html')
-
+    return render(request, 'bag/bag.html', context)
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -29,6 +35,47 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'{product.name} is added to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        bag[item_id]['items_by_size'][size] = quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
