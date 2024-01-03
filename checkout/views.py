8d33d153f37b72a88ac314933fff14f174db42cf
Ire <123773847+irebernardidev@users.noreply.github.com>
2024-01-03 11:43:18 +0000

    Integrate Stripe Functionality

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OUSM3AORmqtXqj80RaeAuWptbHpZIsxjN6ia7ZAf8YxKUb8W0bYBZrkZhr7IFDYrsv2xFUh7NhCe3eU9RtMk4Z1008MemLpbn',
+        'client_secret': 'test client secret', 
     }
 
     return render(request, template, context)
