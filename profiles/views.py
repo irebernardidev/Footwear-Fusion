59f88eb8b60e91c1b3055c36085fb88b1d878c3f
Ire <123773847+irebernardidev@users.noreply.github.com>
2024-01-08 20:17:02 +0000

    Enhance Product Management: Implement Editing and Deletion Features for Products, Strengthen Security for Product Administration Interfaces.

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from .models import UserProfile
-from .forms import UserProfileForm
+from django.contrib.auth.decorators import login_required
+
 from checkout.models import Order
 from reviews.models import Review
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
 
