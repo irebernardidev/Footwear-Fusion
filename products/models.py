c398c06f4a4a44899ab8fc7c1d441fa59523e494
Ire <123773847+irebernardidev@users.noreply.github.com>
2024-01-08 16:35:34 +0000

    Add a new column 'total_purchased' to the product table and ensure it is updated with each product purchase transaction.

--- products/models.py
+++ products/models.py
@@ -1,24 +1,39 @@
 from django.db import models
+
+
 class Category(models.Model):
+    """ Category Model (Women, Men, Kids) """
+
     class Meta:
         verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    
     def __str__(self):
         return self.name
+    
     def get_friendly_name(self):
         return self.friendly_name
+
+
 class Subcategory(models.Model):
+    """ Subcategory Model (e.g. Sneakers, Boots, Sale, etc)"""
+
     class Meta:
         verbose_name_plural = 'Subcategories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     def __str__(self):
         return self.name
     def get_friendly_name(self):
         return self.friendly_name
-    
+
+
 class Product(models.Model):
+    """ Product Model """
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     subcategory = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
@@ -27,5 +42,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    total_purchased = models.IntegerField(default=0, null=False, blank=False)
+
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -8,9 +8,11 @@ from favorites.models import Favorites, FavoritesItem
 from reviews.models import Review
 from .models import Product, Category, Subcategory
 
+
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    products = Product.objects.all()[0:24]
+
+    products = Product.objects.all()
     total_obj = Product.objects.count()
     query = None
     categories = Category.objects.all()
@@ -26,7 +28,7 @@ def all_products(request):
             favorites = Favorites.objects.get(user=request.user)
         except Favorites.DoesNotExist:
             pass
-
+        
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -34,37 +36,48 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+                
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
+            
         if 'category' in request.GET:
             var_category = request.GET['category']
             categories = request.GET['category'].split(',')
+            
             if 'subcategory' in request.GET:
                 var_subcategory = request.GET['subcategory']
                 subcategories = request.GET['subcategory'].split(',')
                 products = products.filter(category__name__in=categories, subcategory__name__in=subcategories)
                 subcategories = Subcategory.objects.filter(name__in=subcategories)
+                
             else:
                 products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
+            
         if 'subcategory' in request.GET:
             var_subcategory = request.GET['subcategory']
             subcategories = request.GET['subcategory'].split(',')
             products = products.filter(subcategory__name__in=subcategories)
             subcategories = Subcategory.objects.filter(name__in=subcategories)
+            
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
+            
             # ADD DESCRIPTION TO QUERY ?????
             queries = Q(name__icontains=query)
             products = products.filter(queries)
+
+    else:
+        products = products[0:24]
+
     current_sorting = f'{sort}_{direction}'
-  
+
     if request.user.is_authenticated:
         context = {
             'products': products,
@@ -88,6 +101,7 @@ def all_products(request):
             'var_category': var_category,
             'var_subcategory': var_subcategory,
         }
+        
     # PRINT VARIABLES, DON'T FORGET TO REMOVE
     # print("var_category: ", var_category)
     # print("var_subcategory: ", var_subcategory)
@@ -96,32 +110,34 @@ def all_products(request):
     # print("current_sorting: ", sort)
     # print("favorites_item", favorites_item)
     print("favorites", favorites)
-
+    
     return render(request, 'products/products.html', context)
+
 
 def load_more(request):
     """Load more products"""
-
     loaded_item = request.GET.get('loaded_item')
     loaded_item_int = int(loaded_item)
     limit = 12
     product_obj = list(Product.objects.values()[loaded_item_int:loaded_item_int+limit])
+    loaded_item_after = loaded_item_int + limit
     data = {
-        'products': product_obj
+        'products': product_obj,
+        'loaded_item_after': loaded_item_after
     }
+    print(loaded_item_after)
     return JsonResponse(data=data)
 
 
 def product_detail(request, product_id):
     """ A view to show individual product details and the reviews"""
-
+    
     product = get_object_or_404(Product, pk=product_id)
     reviews = Review.objects.filter(product__id__in=product_id)
-
     sizes_women = range(36, 44)
     sizes_men = range(40, 47)
     sizes_kids = range(23, 36)
-
+    
     context = {
         'product': product,
         'reviews': reviews,
