6c41ca59c2e2522a513f12c6c2fcd8f32e00f666
Ire <123773847+irebernardidev@users.noreply.github.com>
2024-01-08 20:02:33 +0000

    Update Product Admin: Implement functionality for adding new products to the database, develop corresponding views and URLs, and introduce templates for product addition.

--- products/views.py
+++ products/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.core import serializers
 from django.http import JsonResponse
 from django.db.models import Q
 from django.db.models.functions import Lower
-from favorites.models import Favorites, FavoritesItem
+from favorites.models import Favorites
 from reviews.models import Review
 from .models import Product, Category, Subcategory
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -147,3 +147,23 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
