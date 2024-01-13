6265dd33ec985867bdf0493201a7f834977b8aad
Ire <123773847+irebernardidev@users.noreply.github.com>
2024-01-13 12:02:50 +0000

    Enhanced profile page template for better responsiveness and accessibility; included minor CSS adjustments.

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
 from checkout.models import Order
 from reviews.models import Review
 from .models import UserProfile
@@ -13,26 +14,27 @@ SUCCESS_NO_BAG = 50
 @login_required
 def profile(request):
     """ Display the user's profile. """
-    
+
     profile = get_object_or_404(UserProfile, user=request.user)
-    reviews = Review.objects.filter(user=profile).values_list('product__id', flat=True)
-    
+    reviews = Review.objects.filter(user=profile).values_list(
+        'product__id', flat=True)
+
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.add_message(request, SUCCESS_NO_BAG, 'Profile updated successfully')
+            messages.add_message(request, SUCCESS_NO_BAG,
+                                 'Profile updated successfully')
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
-    
+
     template = 'profiles/profile.html'
     context = {
         'profile': profile,
         'form': form,
         'orders': orders,
         'reviews': reviews,
-        #'toast_without_bag': True,
     }
 
     return render(request, template, context)
@@ -40,18 +42,18 @@ def profile(request):
 
 def order_history(request, order_number):
     """ Display the order history """
-    
+
     order = get_object_or_404(Order, order_number=order_number)
-    
+
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. '
         'A confirmation email was sent on the order date.'
     ))
-    
+
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
         'from_profile': True,
     }
-    
+
     return render(request, template, context)
