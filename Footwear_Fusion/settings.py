e65a3aac6e2a7cc6cce25e8721dd1a6afb817f9d
Ire <123773847+irebernardidev@users.noreply.github.com>
2024-01-16 13:07:05 +0000

    Updated middleware configs

--- Footwear_Fusion/settings.py
+++ Footwear_Fusion/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Footwear_Fusion.middleware.AllowIframingMiddleware',
+    'middleware.AllowIframingMiddleware',
 ]
 
 ROOT_URLCONF = 'Footwear_Fusion.urls'
