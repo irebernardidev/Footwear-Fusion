5aee3246282e2eed195dbae7f6ff04b9ada1b8d2
Ire <123773847+irebernardidev@users.noreply.github.com>
2024-01-18 20:03:03 +0000

    Deleted the `middleware.py` file from the root directory since the X-Frame issue was resolved using a Google Chrome extension.

--- Footwear_Fusion/settings.py
+++ Footwear_Fusion/settings.py
@@ -73,7 +73,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.AllowIframingFromAmIResponsiveMiddleware',
 ]
 
 ROOT_URLCONF = 'Footwear_Fusion.urls'
